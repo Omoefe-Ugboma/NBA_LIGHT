de26911c5a85366fc44251e16c4a60eb3083f88a
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-10-27 23:21:57 +0100

    Initial home, Header, Footer and Featured components

--- nba_sport/src/components/App.js
+++ nba_sport/src/components/App.js
@@ -1,10 +1,24 @@
+import React, { Component } from "react";
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      Welcome
-    </div>
-  );
+//COMPONENT
+import Header from './header'
+import Footer from './footer'
+import Home from './home'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/footer" element={<Footer />} />
+        </Routes>
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ nba_sport/src/components/featured.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Featured = (props) => {
+
+    console.log(props)
+
+    return (
+        <div>Slider</div>
+    )
+}
+
+export default Featured;
