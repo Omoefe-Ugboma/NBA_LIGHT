9a362cf814e969300f6f50bcd0abbd02b59afc16
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-10-30 23:44:47 +0100

    Slider Section completed

--- nba_sport/src/components/featured.js
+++ nba_sport/src/components/featured.js
@@ -1,11 +1,52 @@
 import React from "react";
+import Slider from 'react-slick'
+
+const settings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slideToScroll: 1
+}
+
+const generateSlides = ({ slides }) => {
+    if (slides) {
+        return (
+            <Slider {...settings}>
+                {slides.map((item) => {
+                    return (
+                        <div key={item.id}
+                        >
+                            {/* <img src={`/images/covers/${item.cover}`} alt="slide" /> */}
+
+                            <div className="item-slider"
+                                style={{ backgroundImage: `url(/images/covers/${item.cover})` }}
+                            >
+                                <div className="caption">
+                                    <h4>{item.topic}</h4>
+                                    <p>{item.title}</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    )
+                })
+                }
+
+            </Slider >
+        )
+    }
+}
 
 const Featured = (props) => {
 
     console.log(props)
 
     return (
-        <div>Slider</div>
+        <div>
+            {generateSlides(props)}
+        </div>
     )
 }
 
